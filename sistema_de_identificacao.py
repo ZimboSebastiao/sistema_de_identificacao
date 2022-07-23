f905db03bc8f4ed94dfea8a3b6f2b9fd2f6ffc10
Zimbo <gloriasebastiao7@gmail.com>
2022-07-23 17:15:12 -0300

    Create sistema_de_identificacao.py

---
+++ sistema_de_identificacao.py
@@ -0,0 +1,19 @@
+print("***************************")
+print("Sistema de Indentificação")
+print("***************************")
+
+usuario = input("Informe o usuário do sistema: ")
+
+
+print(usuario)
+
+if(usuario == "flavio"):
+    print("Seja bem-vindo Flávio!")
+elif(usuario == "douglas"):
+    print("Seja bem-vindo Douglas!")
+elif(usuario == "nico"):
+    print("Seja bem-vindo Nico!")
+else:
+    print("Usuário não identificado!")
+
+
